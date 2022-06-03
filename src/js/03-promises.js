175ce1dd2d1ebd56f95e8a41d4eaa8c86bf6fd9c
IlyaSV99 <ilavelicko81@gmail.com>
2022-06-03 22:24:34 +0300

    files of hw 9

---
+++ src/js/03-promises.js
@@ -0,0 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+
+const form = document.querySelector('.form');
+const { delay, amount, step } = form.elements;
+
+
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  let counter = 0;
+  let pos = 1;
+  while (counter < amount.valueAsNumber) {
+
+    createPromise(pos, delay.valueAsNumber).then(({position, timeDelay}) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${timeDelay}ms`);
+  })
+    .catch(({position, timeDelay}) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${timeDelay}ms`);
+    })
+
+    counter += 1;
+    pos += 1;
+    delay.valueAsNumber += step.valueAsNumber;
+  }
+}
+
+function createPromise(position, timeDelay) { 
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3; 
+      if (shouldResolve) {
+        resolve({position, timeDelay});
+      } else {
+        reject({position, timeDelay});
+      }
+    }, timeDelay);
+  });
+  return promise;
+}
